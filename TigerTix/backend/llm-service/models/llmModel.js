d7512358c21ad290b0574fcdc3cd6ae766862433
EliMonroe123 <emonro2@clemson.edu>
2025-10-24 16:52:49 -0400

    Added cleanInput function code. Did not use AI response method.

--- TigerTix/backend/llm-service/models/llmModel.js
+++ TigerTix/backend/llm-service/models/llmModel.js
@@ -1,9 +1,9 @@
-import {GoogleGenAI} from '@google/genai';
+import { GoogleGenAI } from '@google/genai';
 import fs from 'node:fs';
 import { parse } from 'node:path';
 
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-const ai = new GoogleGenAI({apiKey: GEMINI_API_KEY});
+const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 const model = 'gemini-2.0-flash-001';
 
 
@@ -14,13 +14,34 @@ async function authenticateWithGoogle() {
     // good luck :)
 
 }
-
+/**
+ * Cleans and validates user input, removing nonalphanumeric characters
+ * @param {*} userInput the string that is entered by the user
+ * @returns {Promise<string>} A promise that resolves with the cleaned string OR
+ * rejects with an error message if the input is invalid.
+ */
 function cleanInput(userInput) {
-    // remove any non alphanumerical characters
-    // possible: send another ai request to determine if the user response is relevant
-    // use a promise here: if user response invalid / inappropiate reject it
-    // return string 
-    return userInput;
+    return new Promise((resolve, reject) => {
+        // use a promise here: if user response invalid / inappropiate reject it
+        //non-string:
+        if (typeof userInput !== 'string') {
+            return reject(new Error('Input must be a string.'));
+        }
+        //empty string:
+        if (typeof userInput === '') {
+            return reject(new Error('No input was entered'));
+        }
+
+        // remove any non alphanumerical characters
+        const cleanedStr = userInput.trim().replace(/[^a-z0-9\s]/gmi, '');
+        //No valid characters rejection:
+        if (cleanedStr === '') {
+            return reject(new Error('No valid characters were input'));
+        }
+
+        // return string
+        resolve(cleaned);
+    });
 }
 
 function parseModelResponse(modelResponse) {
@@ -28,14 +49,14 @@ function parseModelResponse(modelResponse) {
     // turn the ai response into json, singling out event and number of tickets to buy
     // HINT: you can define output structure in prompt.txt...
 
-    return {event: "Jazz Night", tickets: 2}
+    return { event: "Jazz Night", tickets: 2 }
 }
 
 
 export const queryChatbot = async (userInput) => {
     // this function is missing some features
     // HINT: wrap the function in a promise...
-    
+
     userInput = userInput ? userInput : "hi";
     // take user string
     // clean input 
@@ -46,7 +67,7 @@ export const queryChatbot = async (userInput) => {
 
     //TODO: If promise is rejected, send response that request is inappropriate
     const cleanedInput = cleanInput(userInput);
-    
+
     const prompt = await new Promise((resolve, reject) => {
         fs.readFile('./prompt.txt', 'utf-8', (error, data) => {
             if (error) {
@@ -57,7 +78,7 @@ export const queryChatbot = async (userInput) => {
             resolve(data);
             return;
         });
-        
+
     });
 
     console.log(prompt);
