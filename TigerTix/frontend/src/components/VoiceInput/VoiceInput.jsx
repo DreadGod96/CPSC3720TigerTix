14cfce2e5a33d2a57f3f160f710f153196e6016b
EliMonroe123 <emonro2@clemson.edu>
2025-10-25 10:14:50 -0400

    Added browser compatibility check, and beep sound with Web Audio API

--- TigerTix/frontend/src/components/VoiceInput/VoiceInput.jsx
+++ TigerTix/frontend/src/components/VoiceInput/VoiceInput.jsx
@@ -1,3 +1,41 @@
-import "./VoiceInput.css"
-import React, { useState, useEffect } from "react";
+import "./VoiceInput.css";
+import React, { useState, useEffect } from 'react';
+import { FAMicrophonbe } from 'react-icons/fa';
 
+//Ensure browser compatibility
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition; let recognition;
+if (SpeechRecognition) {
+    recognition = new SpeechRecognition();
+    recognition.interimResults = false;
+    recognition.continuous = false;
+    recognition.lang = 'en-US';
+    recognition.maxAlternatives = 1;
+}
+else {
+    console.log("Web Speech API is not supported by this browser.");
+}
+
+//Create beep sound with Web Audio API
+const playBeep = () => {
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const oscillator = audioContext.createOscillator();
+    oscillator.type = 'square';
+    oscillator.frequency.setValueAtTime(440, audioContext.currentTime);
+    oscillator.connect(audioContext.destination);
+    oscillator.start();
+    setTimeout(() => { oscillator.stop(); }, 200);
+};
+
+
+const VoiceInput = ({ onSpeechResult }) => {
+
+    const handleMicClick = () => {
+
+    };
+
+    useEffect(() => {
+
+    })
+};
+
+export default VoiceInput;
