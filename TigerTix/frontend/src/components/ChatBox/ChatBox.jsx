d4cf4eea5484890b5e40d8945fcbae1769a7907c
Roman Pasqualone <romdpas0731@gmail.com>
2025-11-02 23:19:08 -0500

    Fix: Fixed issue with data not showing properly

--- TigerTix/frontend/src/components/ChatBox/ChatBox.jsx
+++ TigerTix/frontend/src/components/ChatBox/ChatBox.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './ChatBox.css';
 import { FaRobot, FaUser, FaPaperPlane } from 'react-icons/fa';
 import VoiceInput from '../VoiceInput/VoiceInput';
