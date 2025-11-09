2f2395d25f39cc6a3ffe3520fc3790b510e7975f
EliMonroe123 <emonro2@clemson.edu>
2025-11-09 13:25:41 -0500

    FIX: syntax error, needed semi colons

--- TigerTix/backend/shared-db/init.sql
+++ TigerTix/backend/shared-db/init.sql
@@ -4,10 +4,10 @@ CREATE TABLE IF NOT EXISTS events (
     event_date date,
     number_of_tickets_available int,
     price_of_a_ticket decimal
-)
+);
 
 CREATE TABLE IF NOT EXISTS users (
     user_id INTEGER PRIMARY KEY,
     email varchar(255) NOT NULL UNIQUE,
     password TEXT NOT NULL
-)
+);
