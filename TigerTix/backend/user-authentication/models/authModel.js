d9ff49041e17f3dcecaf5fe1742f73ff2ddca8ee
EliMonroe123 <emonro2@clemson.edu>
2025-11-09 11:44:34 -0500

    Wrote the findUser function and contracts for findUser and createUser

---
+++ TigerTix/backend/user-authentication/models/authModel.js
@@ -0,0 +1,45 @@
+import sqlite3 from "sqlite3";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const IS_TEST_ENV = process.env.NODE_ENV === 'test';
+const DATABASE_NAME = IS_TEST_ENV ? 'test-database.sqlite' : 'database.sqlite';
+const DATABASE_PATH = path.join(__dirname, '..', '..', 'shared-db', DATABASE_NAME);
+
+const sqlite_3 = sqlite3.verbose();
+
+const database = new sqlite_3.Database(DATABASE_PATH, sqlite3.OPEN_READWRITE, (error) => {
+    if (error) {
+        console.error("Authentication Model Error: Failed to connect to the database.", error.message);
+    }
+});
+
+/**
+ * Finds a user in the database using their email address
+ * @param {string} email The user's email address
+ * @returns {Promise<object|undefined>} A promise that resolves with the user row if found by the query,
+ * or 'undefined' if no row is found that matches the query. Rejects with an error. 
+ */
+export const findUser = (email) => {
+    return new Promise((resolve, reject) => {
+        const sql_cmd = `SELECT * FROM users WHERE email = ?`
+
+        database.get(sql_cmd, [email], (error,row) => {
+            if (error) reject(error);
+            else resolve(rows);
+        });
+    });
+};
+
+/**
+ * Creates a new user in the database
+ * @param {string} email The new user's email address
+ * @param {string} password The new user's already hashed password
+ * @returns {Promise<{id: number, email: string}>} A promise that resolves with the new user's id and email 
+ */
+export const createUser = (email, password) => {
+    
+};
+
