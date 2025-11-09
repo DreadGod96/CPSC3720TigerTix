9529af54bb2f8173c7bcf4f7638f564046d6ad76
EliMonroe123 <emonro2@clemson.edu>
2025-11-09 10:31:13 -0500

    Added users table

--- TigerTix/backend/shared-db/init.sql
+++ TigerTix/backend/shared-db/init.sql
@@ -5,3 +5,9 @@ CREATE TABLE IF NOT EXISTS events (
     number_of_tickets_available int,
     price_of_a_ticket decimal
 )
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY,
+    email varchar(255), NOT NULL UNIQUE
+    password TEXT NOT NULL
+)
