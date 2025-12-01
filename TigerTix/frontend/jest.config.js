8636fdafe432b519e83713cbf82a12f0dff8e84d
Roman Pasqualone <romdpas0731@gmail.com>
2025-12-01 16:58:43 -0500

    Add: Added jest.config.js for e2e test

--- TigerTix/frontend/jest.config.js
+++ TigerTix/frontend/jest.config.js
@@ -1,7 +1,13 @@
+// frontend/jest.config.js
 /** @type {import('jest').Config} */
 const config = {
-    setupFiles: ['<rootDir>/src/setupTests.js']
-  verbose: true,
+  setupFiles: ['<rootDir>/src/setupTests.js'],
+verbose: true,
+// Attempt to resolve module resolution issues with react-router packages
+moduleNameMapper: {
+  "^react-router-dom$": "<rootDir>/node_modules/react-router-dom",
+  "^react-router$": "<rootDir>/node_modules/react-router",
+},
 };
 
 module.exports = config;
